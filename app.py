0c885fc7df5e081949142698d46133ab6e89764e
andreascmbuch <anbu0001@stud.kea.dk>
2024-12-04 19:51:30 +0100

    Ny ændringer til app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import sqlite3
-from flask import Flask, request, jsonify, Response
-
-
+from flask import Flask, jsonify
 
 def get_db_connection():
-    return sqlite3.connect('damage_database.db')
+    conn = sqlite3.connect('damage_database.db')
+    conn.row_factory = sqlite3.Row  
+    return conn
 
 app = Flask(__name__)
 
@@ -13,9 +13,13 @@ def list_of_car_damage():
     conn = get_db_connection()
     cursor = conn.cursor()
     cursor.execute('SELECT * FROM damage')
-    damage = cursor.fetchall()
+    damage_records = cursor.fetchall()
     conn.close()
-    return jsonify(damage)
+
+   
+    damage_list = [dict(row) for row in damage_records]
+
+    return jsonify(damage_list)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
