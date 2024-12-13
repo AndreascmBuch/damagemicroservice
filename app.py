f371983471e206ffda41ccd7ae1e33fe647582af
andreascmbuch <anbu0001@stud.kea.dk>
2024-12-13 13:46:55 +0100

    DB_path igen igen

--- app.py
+++ app.py
@@ -3,12 +3,14 @@ import sqlite3
 from flask import Flask, jsonify, request
 from dotenv import load_dotenv
 
+DB_PATH = os.getenv('DB_PATH', 'damage_database.db')
+
 def get_db_connection():
-    conn = sqlite3.connect('damage_database.db')
+    conn = sqlite3.connect('DB_PATH')
     conn.row_factory = sqlite3.Row  
     return conn
 
-DB_PATH = os.getenv('DB_PATH', 'damage_database.db')
+
 
 app = Flask(__name__)
 
